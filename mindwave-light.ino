f2707b0e2f74408cda3aa4f4aab52e3fa3fa6548
AceLin <echislin123@live.com>
2016-12-20 17:33:54 +0800

    加入指示灯，有点乱，下一版本先调整，再加入指示灯

--- mindwave-light.ino
+++ mindwave-light.ino
@@ -14,6 +14,9 @@
  #include "BLE.h"
  #include "debug.h"
 
+/*** 配置低功耗 ***/
+#include<Enerlib.h>
+
  #define DEBUG
  #define mwLight_DEBUG
 
@@ -42,6 +45,8 @@ MODE_t chooseOneMode = MEDITATION;
 /*** 大灯专有属性，按键切换模式，进入关闭状态（睡眠） ***/
 typedef enum{ SLEEP = 0, WORK }sw_t;
 sw_t Sleep_or_Work = WORK;                 // 定义初始状态在工作状态
+uint16_t change_moment = 0;                // 标志此时是否处于切换状态的瞬间，如果是，指示灯需要针对闪烁
+void OFF_Light();
 
 #if 0
   SoftwareSerial mySerial =  SoftwareSerial(8, 7);
@@ -55,6 +60,17 @@ sw_t Sleep_or_Work = WORK;                 // 定义初始状态在工作状态
 
 /*** 定义蓝牙模块在本实例电路中连接的引脚 ***/
 BLE_module_t BLElight;            // 在定义一个struct 对象时，如何初始化？
+
+
+/*** 加入低功耗 *****/
+Energy lowPower;                        // 定义低功耗类对象
+const uint16_t whether_Sleep_pin = 3;   // pin3读取电平状态判断是否需要进入睡眠模式
+const uint16_t ctrlSleep_pin = 17;      // pin17(A3) 根据chooseOneMode输出高低
+void lowPower_pin_init(){
+    pinMode( whether_Sleep_pin, INPUT_PULLUP );
+    pinMode( ctrlSleep_pin, OUTPUT );
+    digitalWrite( ctrlSleep_pin, HIGH );
+}
 
 
 void setup(){
@@ -81,8 +97,10 @@ void setup(){
 
   // add KEY_MODE
   forceFlush();
+  
+  lowPower_pin_init();                        /* 低功耗模式引脚控制 */
   pinMode( KEY_MODE, INPUT_PULLUP );
-  attachInterrupt( digitalPinToInterrupt( KEY_MODE ), chooseMode, FALLING);
+  attachInterrupt( digitalPinToInterrupt( KEY_MODE ), chooseMode, FALLING);   // LOW-低电平触发中断会出现问题，多次进入中断
   forceFlush();
 }
 
@@ -92,15 +110,52 @@ color_t lightColor = FOOCOLOR;              // RBG灯对象，设定颜色即可
 uint8_t Rx_data;                            // 从连接蓝牙模块的虚拟串口接收到的数据
 /***************************   loop()   *************************/
 void loop(){
-    if ( Sleep_or_Work == SLEEP ){                            // 进入低功耗模式
+    if ( Sleep_or_Work == SLEEP ){ // 进入低功耗模式
+        if ( change_moment == 1 ){  // 如果这个按键更改模式的时刻...
+          OFF_Light();   delay_ms( 1500); 
+          OFF_Light();
+          change_moment = 0;
+        }
         digitalWrite ( BLElight.EN_PIN, LOW );                // 这一句后期调整成面向对象模式
+        digitalWrite( ctrlSleep_pin, LOW );
         if( chooseOneMode == TURN_OFF ){
             // digitalWrite( 11, HIGH );
             lightColor = FOOCOLOR;
             setLightColor( FOOCOLOR );
+            if( digitalRead( whether_Sleep_pin ) == LOW )
+                Serial.println( "Enter into SLEEP in... ");
+                Serial.println( " 3 ");
+                Serial.println( " 2 ");
+                Serial.println( " 1 ");
+                lowPower.PowerDown();                            // 进入低功耗
         } // end if TURN_OFF 
     }
     else if ( Sleep_or_Work == WORK ){
+          if( change_moment == 1 ){
+            switch( chooseOneMode ){
+              case MEDITATION:
+                    change_moment = 1;
+                    lightColor = BLUE;
+                    setLightColor( FOOCOLOR );  delay_ms( 1000 );
+                    setLightColor( lightColor );    delay_ms( 1000 );
+                    setLightColor( FOOCOLOR );  delay_ms( 1000 );
+                    setLightColor( lightColor );    delay_ms( 1000 );
+                    setLightColor( FOOCOLOR );  delay_ms( 1000 );
+                    setLightColor( lightColor );
+                    break;
+              case ATTENTION:
+                    lightColor = YELLOW;
+                    setLightColor( FOOCOLOR );  delay_ms( 1000 );
+                    setLightColor( lightColor );    delay_ms( 1000 );
+                    setLightColor( FOOCOLOR );  delay_ms( 1000 );
+                    setLightColor( lightColor );    delay_ms( 1000 );
+                    setLightColor( FOOCOLOR );  delay_ms( 1000 );
+                    setLightColor( lightColor );  
+                    break;
+            } // end switch()
+            change_moment = 0;
+          }
+          digitalWrite( ctrlSleep_pin, HIGH );
           digitalWrite ( BLElight.EN_PIN, HIGH );                // 打开蓝牙
           if( ifSuccess_got_data()== FAIL ){
             if( Serial.available() > 0 ){
@@ -148,12 +203,47 @@ void loop(){
  }
 #endif /* debug */
 
+void blinkLight( color_t _color, uint16_t _period_ms ){
+  uint16_t i=0, j= 1000/_period_ms;
+  for ( i=0; i<j; i++){
+    setLightColor( FOOCOLOR );  delay_ms( _period_ms );
+    setLightColor( _color );    delay_ms( _period_ms );
+  }
+}
+
+void breatheLight( color_t _color ){
+  uint16_t i=0, times = 10;
+  for( i=0; i<5; i++){   //  越亮
+      blinkLight( _color, times );
+      //delay_ms( times );
+      times += 10;
+  }
+  setLightColor( _color );
+  for( i=0; i<50; i++){ //  越暗
+      setLightColor( _color );
+      delay_ms( 10 );
+      setLightColor( FOOCOLOR );
+      delay_ms( times );
+      times += 10;
+  }
+  setLightColor( FOOCOLOR );
+}
+void OFF_Light(){
+    setLightColor( FOOCOLOR ); delay_ms( 800 ); 
+    setLightColor( ORANGE );  delay_ms(1000);
+    setLightColor( FOOCOLOR ); delay_ms( 1000 ); 
+    setLightColor( ORANGE );  delay_ms(1000);
+    setLightColor( FOOCOLOR );
+}
+
+
 void chooseMode(void){
   static MODE_t lastMode = MEDITATION;
-  forceFlush();
   delay_ms( 10 );
   forceFlush();
+
   if ( digitalRead(KEY_MODE) == LOW ){
+      if( lowPower.WasSleeping() )  delay(10);        // 推出低功耗模式
 
       switch ( lastMode ){
       case MEDITATION:
@@ -162,6 +252,7 @@ void chooseMode(void){
                     chooseOneMode = ATTENTION; 
                     lastMode = ATTENTION;
                     Serial.println( "get into Attention mode " );
+                    change_moment = 1;
                     break;
       case ATTENTION: 
                     forceFlush();
@@ -169,6 +260,7 @@ void chooseMode(void){
                     chooseOneMode = TURN_OFF; 
                     lastMode = TURN_OFF;
                     Serial.println( "get into low power mode" );
+                    change_moment = 1;
                     break;
 
       case TURN_OFF: 
